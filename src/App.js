76e3ff68a63c27ea9702088f05fb9dabbf98e255
David Walsh <davo.walsh@gmail.com>
2016-11-15 16:41:03 +0000

    css+appupdate

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import Reactable from 'reactable';
 import allFighters from './Data'
+import './app.css' ;
 
 var Table = Reactable.Table,
     Thead = Reactable.Thead,
     Th = Reactable.Th;
 
 
+
+
+
 class FighterList extends React.Component {
-
-
-
   render() {
     return (<div className="table">
       <Table className="table"
         filterable={['name', 'weight', 'record']}
         noDataText="No matching records found"
-        itemsPerPage={10}
+        itemsPerPage={5}
         currentPage={0}
         sortable={true}
         data={allFighters}>
         <Thead>
-          <Th column="name">Name</Th>
+          <Th column="name">Date</Th>
           <Th column="weight">Weight</Th>
           <Th column="record">Record</Th>
+		  <Th column="img">CountryIcon</Th>
          
         </Thead>
       </Table>
@@ -35,5 +37,4 @@ class FighterList extends React.Component {
 
 }
 export default FighterList;
-
   
--- src/Data.js
+++ src/Data.js
@@ -1,16 +1,32 @@
 
  var AllFighters = 
               [ 
-			    {name: "Conor McGregor", weight: "145/155", record: "Exploration", members: 4},
-				{name: "Frankie Edgar", weight: "145", record: "Search and Rescue", members: 5},
-				{name: "Demetrious Johnson", weight: "125", record: "Marine Combat", members: 10},
-				{name: "Dominick Cruz", weight: "135", record: "Medical", members: 4},
-				{name: "Daniel Cormier", weight: "205", record: "Marine Combat", members: 6},
-				{name: "Stipe Miocic", weight: "232", record: "Search and Rescue", members: 10},
-				{name: "Eddie Alvarez", weight: "155", record: "Scientific", members: 6},
-				{name: "Joanna Jedrzejczyk", weight: "115", record: "Medical", members: 6},
-				{name: "Tyron Woodley", weight: "170", record: "Diplomatic", members: 7},
-				{name: "Michael Bisping", weight: "180", record: "Military Exploration", members: 5}
+			    {name: "29/10/16", weight: "145/155", record: "Exploration", img: "./pics/usa.png"},
+				{name: "29/10/16", weight: "145", record: "Search and Rescue", img: 5},
+				{name: "29/10/16", weight: "125", record: "Marine Combat", img: 10},
+				{name: "29/10/16", weight: "135", record: "Medical", img: 4},
+				{name: "29/10/16", weight: "205", record: "Marine Combat", img: 6},
+				{name: "29/10/16", weight: "232", record: "Search and Rescue", img: 10},
+				{name: "29/10/16", weight: "155", record: "Scientific", img: 6},
+				{name: "29/10/16", weight: "115", record: "Medical", img: 6},
+				{name: "29/10/16", weight: "170", record: "Diplomatic", img: 7},
+				{name: "29/10/16", weight: "180", record: "Military Exploration", img: 5}
+			   
+			] ; 
+			
+			
+ var AllFighte = 
+              [ 
+			    {name: "Conor McGregor", weight: "145/155", record: "Exploration", img: "./pics/usa.png"},
+				{name: "Frankie Edgar", weight: "145", record: "Search and Rescue", img: 5},
+				{name: "Demetrious Johnson", weight: "125", record: "Marine Combat", img: 10},
+				{name: "Dominick Cruz", weight: "135", record: "Medical", img: 4},
+				{name: "Daniel Cormier", weight: "205", record: "Marine Combat", img: 6},
+				{name: "Stipe Miocic", weight: "232", record: "Search and Rescue", img: 10},
+				{name: "Eddie Alvarez", weight: "155", record: "Scientific", img: 6},
+				{name: "Joanna Jedrzejczyk", weight: "115", record: "Medical", img: 6},
+				{name: "Tyron Woodley", weight: "170", record: "Diplomatic", img: 7},
+				{name: "Michael Bisping", weight: "180", record: "Military Exploration", img: 5}
 			   
 			] ; 
 
