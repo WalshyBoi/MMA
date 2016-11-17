d2dde1ca4ba8945734c37605ede6a21d38162d9b
David Walsh <davo.walsh@gmail.com>
2016-11-17 19:59:23 +0000

    MajorUIOverhaul

--- src/App.js
+++ src/App.js
@@ -2,37 +2,124 @@ import React from 'react';
 import Reactable from 'reactable';
 import allFighters from './Data'
 import './app.css' ;
+import { Navbar, NavItem, NavDropdown, MenuItem, Nav } from 'react-bootstrap';
+import {Router, Route, browserHistory, Link} from 'react-router';
 
 var Table = Reactable.Table,
     Thead = Reactable.Thead,
     Th = Reactable.Th;
 
-class Header extends React.Component{
-	render(){
-		return(
-			
-			<header>{this.props.header}</header>
-		);
-		
-	}
-	
-}
 
-class Fighter extends React.Component{
+
+
+
+
+class FighterBio extends React.Component{
   render() {
-   
+   return(
+   <div className="Bio">
+   <li>
+      <img src={require("./pics/mma.png")} />
+
+   </li>
+   </div>
+
+
+   )
 
   }
 }
 
+
+
+const NavBarConstant = React.createClass({
+  handleSelect(eventKey) {
+    event.preventDefault();
+    alert(`selected ${eventKey}`);
+  },
+
+  render() {
+    return (
+      <div id="navbar">
+      <Nav bsStyle="tabs" activeKey="1" onSelect={this.handleSelect}>
+       
+        <NavItem eventKey="1"><Link to="/">Home</Link></NavItem>
+        <NavItem eventKey="2"><Link to="/featherweight">Feather Weight</Link></NavItem>
+        <NavItem eventKey="3"><Link to="/lightweight">Light Weight</Link></NavItem>
+        <NavItem eventKey="4"><Link to="/welterweight">Welter Weight</Link></NavItem>
+        <NavItem eventKey="5"><Link to="/heavyweight">Heavy Weight</Link></NavItem>
+       
+      </Nav>
+      </div>
+    );
+  }
+});
+
+
+class Pound4Pound extends React.Component {
+  render() {
+    return(
+      <div>
+      <NavBarConstant />
+      
+    </div>
+    );
+    }
+  }
+
+
+
+  class FeatherWeight extends React.Component {
+  render() {
+    return(
+      <div>
+      <NavBarConstant />
+      
+    </div>
+    );
+    }
+  }
+
+  class LightWeight extends React.Component {
+  render() {
+    return(
+      <div>
+      <NavBarConstant />
+      
+    </div>
+    );
+    }
+  }
+
+  class WelterWeight extends React.Component {
+  render() {
+    return(
+      <div>
+      <NavBarConstant />
+      
+    </div>
+    );
+    }
+  }
+
+  class HeavyWeight extends React.Component {
+  render() {
+    return(
+      <div>
+      <NavBarConstant />
+      
+    </div>
+    );
+    }
+  }
        
 
 class FighterList extends React.Component {
   render() {
     return (
 	<div>
-	   <Header header={"Random"} />
-	  
+	   <NavBarConstant />
+      
 	<div className="table">
       <Table className="table"
         filterable={['name', 'weight', 'record']}
@@ -42,20 +129,32 @@ class FighterList extends React.Component {
         sortable={true}
         data={allFighters}>
         <Thead>
-          <Th column="name">Date</Th>
+          <Th column="name">
+            <strong className="name-header">Name</strong>
+          </Th>
+          
           <Th column="weight">Weight</Th>
           <Th column="record">Record</Th>
 		  <Th column="img">CountryIcon</Th>
-         
+
         </Thead>
       </Table>
 	  </div>
 	  </div>
     )
   }
-
-
-
 }
-export default FighterList;
-  
+
+
+ReactDOM.render((
+    <Router history={browserHistory}>
+    <Route path="/" component={FighterList}>
+    <Route path="Home" component={FighterList} />
+    </Route>
+    <Route path="heavyweight" component={HeavyWeight} />
+    <Route path="lightweight" component={LightWeight} />
+    <Route path="welterweight" component={WelterWeight} />
+    <Route path="featherweight" component={FeatherWeight} />
+              
+    </Router>
+), document.getElementById('root'))
--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 //import Fighters from  './Data';
 import FighterList from './App';
+import Pound4Pound from './App';
+import {Router, Route, browserHistory} from 'react-router';
+
+//Link import this shiz
 
 ReactDOM.render((
-  <FighterList />),
-  document.getElementById('root')
-);
+		<Router history={browserHistory}>
+		<Route path="/" component={FighterList}>
+	    <Route path="p4p" component={Pound4Pound} />
+		</Route>
+		</Router>
+), document.getElementById('root'))
