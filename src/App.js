ee25affceb6a74fee7feb748200e6059d321bdff
David Walsh <davo.walsh@gmail.com>
2016-11-18 17:15:32 +0000

    Major Nav/Data rendering model

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import Reactable from 'reactable';
 import {AllFighters} from './Data'
 import {allFeatherWeights} from './Data'
+import {allLightWeights} from './Data'
+import {allWelterWeights} from './Data'
 import './app.css' ;
 import { Navbar, NavItem, NavDropdown, MenuItem, Nav } from 'react-bootstrap';
 import {Router, Route, browserHistory, Link} from 'react-router';
+import {PieChart} from 'react-easy-chart';
+import {LineChart} from 'react-easy-chart';
+import { Timeline } from 'react-twitter-widgets'
+
 
 var Table = Reactable.Table,
     Thead = Reactable.Thead,
     Th = Reactable.Th;
-
-
-
-
 
 
 class FighterBio extends React.Component{
@@ -36,7 +38,7 @@ class FighterBio extends React.Component{
 const NavBarConstant = React.createClass({
   handleSelect(eventKey) {
     event.preventDefault();
-    alert(`selected ${eventKey}`);
+  
   },
 
   render() {
@@ -45,10 +47,10 @@ const NavBarConstant = React.createClass({
       <Nav bsStyle="tabs" activeKey="1" onSelect={this.handleSelect}>
        
         <NavItem eventKey="1"><Link to="/">Home</Link></NavItem>
-        <NavItem eventKey="2"><Link to="/featherweight">Feather Weight</Link></NavItem>
-        <NavItem eventKey="3"><Link to="/lightweight">Light Weight</Link></NavItem>
-        <NavItem eventKey="4"><Link to="/welterweight">Welter Weight</Link></NavItem>
-        <NavItem eventKey="5"><Link to="/heavyweight">Heavy Weight</Link></NavItem>
+        <NavItem eventKey="2"><Link activeClassName="active" to="/featherweight">Feather Weight</Link></NavItem>
+        <NavItem eventKey="3"><Link activeClassName="active" to="/lightweight">Light Weight</Link></NavItem>
+        <NavItem eventKey="4"><Link activeClassName="active" to="/welterweight">Welter Weight</Link></NavItem>
+        <NavItem eventKey="5"><Link activeClassName="active" to="/heavyweight">Heavy Weight</Link></NavItem>
        
       </Nav>
       </div>
@@ -62,6 +64,7 @@ export class Pound4Pound extends React.Component {
     return(
       <div>
       <NavBarConstant />
+
       
     </div>
     );
@@ -71,24 +74,68 @@ export class Pound4Pound extends React.Component {
 
 
  export class FeatherWeight extends React.Component {
-  render() {
+   render() {
     return(
       <div>
       <NavBarConstant />
       
+      
+     <div id="fighter">
+      <Fighter data={allFeatherWeights} />
+      </div>
     </div>
     );
     }
   }
 
+ class LineChartMaker extends React.Component{
+  render(){
+    return(
+  
+   <LineChart
+    axisLabels={{x: 'Total Revenue', y: 'January'}}
+    margin={{top: 10, right: 30, bottom: 50, left: 70}}
+    yType={'time'}
+    axes
+    grid
+    width={500}
+    height={500}
+    data={[
+      [
+        { x: 10, y: '1-Jan-15' },
+        { x: 20, y: '10-Jan-15' },
+        { x: 40, y: '21-Jan-15' },
+        { x: 80, y: '31-Jan-15' }
+      ], [
+        { x: 0, y: '1-Jan-15' },
+        { x: 15, y: '10-Jan-15' },
+        { x: 20, y: '21-Jan-15' },
+        { x: 25, y: '31-Jan-15' }
+      ]
+    ]}
+  />
+  );
+}
+
+ }
  export class LightWeight extends React.Component {
   render() {
     return(
       <div>
       <NavBarConstant />
-      
-     <div id="fighter">
-      <Fighter data={allFeatherWeights} />
+      <div id = "lineFighter"><Timeline
+    dataSource={{
+      sourceType: 'profile',
+      screenName: 'TheNotoriousMMA'
+    }}
+    options={{
+      username: 'TheNotoriousMMA',
+      height: '400'
+    }}
+    onLoad={() => console.log('Timeline is loaded!')}
+  /></div>
+      <div id="fighter">
+      <Fighter data={allLightWeights} />
       </div>
     </div>
     );
@@ -106,9 +153,33 @@ export class Pound4Pound extends React.Component {
       
       <li key={fighter.id} className="thumbnailfighter">
                  
-                 <img src={require(fighter.img)} />
-                 
-                <p>{fighter.name}</p>
+                
+
+ 
+                <h1> <div id="fightertitle">{fighter.name}</div></h1>
+                
+                
+                <img src={require(fighter.img)} />
+                <div id="outer"><p>Win/Loss</p>
+                
+                <div id="pie"><PieChart size={200}
+    labels
+    data={[
+      {key: 'Win', value: fighter.win, color: '#2a6d3b'},
+      {key: 'Loss', value: fighter.lose, color: '#a0092c'}
+    ]}
+    styles={{
+      '.chart_text': {
+        fontSize: '1em',
+        fill: '#fff'
+      }
+    }}
+  /></div></div>
+
+          
+
+ 
+
 
               </li>
 
@@ -117,17 +188,21 @@ export class Pound4Pound extends React.Component {
     })
     return(
      
-    <ul>{namesList}</ul>
+    <div id="grid"><ul>{namesList}</ul></div>
     
     );
     }
   }
+
+
+
 
  export class WelterWeight extends React.Component {
   render() {
     return(
       <div>
       <NavBarConstant />
+      <Fighter data={allWelterWeights} />
       
     </div>
     );
--- src/Data.js
+++ src/Data.js
@@ -17,31 +17,46 @@ export const AllFighters =
 			
  export var allFeatherWeights = 
               [ 
-			    {id: 0, name: "Conor McGregor", record: "21-3-0", height: "5' 7", img: "./pics/FeatherWeight/Conor-McGregor.png"},
-				{id: 1, name: "Jose Aldo", record: "26-2-0", height: "5'8", img: "./pics/FeatherWeight/Jose-Aldo.png"},
-				{id: 2, name: "Frankie Edgar", record: "20-5-2", height: "5'6", img: "./pics/FeatherWeight/Frankie-Edgar.png"},
-				{id: 3, name: "Max Holloway", record: "16-3-0", height: "5'11", img: "./pics/FeatherWeight/Max-Holloway.png"},
-				{id: 4, name: "Ricardo Lamas", record: "17-5-0", height: "5'8", img: "./pics/FeatherWeight/Ricardo-Lamas.png"},
-				{id: 5, name: "Cub Swanson", record: "23-7-0", height: "5'8", img: "./pics/FeatherWeight/Cub-Swanson.png"},
-				{id: 6, name: "Anthony Pettis", record: "19-5-0", height: "5'10", img: "./pics/FeatherWeight/Anthony-Pettis.png"},
-				{id: 7, name: "Jeremy Stephens", record: "25-13-0", height: "5'9", img: "./pics/FeatherWeight/Jeremy-Stephens.png"},
-				{id: 8, name: "Charles Oliviera", record: "21-7-0", height: "5'10", img: "./pics/FeatherWeight/Charles-Oliveria.png"},
-				{id: 9, name: "Dennis Bermudez", record: "17-5-0", height: "5'6", img: "./pics/FeatherWeight/Dennis-Bermudez.png"}
+			    {id: 0, name: "(C)Conor McGregor", win: 21, lose:3, height: "5' 7", img: "./pics/FeatherWeight/Conor-McGregor.png"},
+				{id: 1, name: "Jose Aldo", win: 26,lose:2, height: "5'8", img: "./pics/FeatherWeight/Jose-Aldo.png"},
+				{id: 2, name: "Frankie Edgar", win: 20,lose:5, height: "5'6", img: "./pics/FeatherWeight/Frankie-Edgar.png"},
+				{id: 3, name: "Max Holloway", win: 16,lose:3, height: "5'11", img: "./pics/FeatherWeight/Max-Holloway.png"},
+				{id: 4, name: "Ricardo Lamas", win: 17,lose:5, height: "5'8", img: "./pics/FeatherWeight/Ricardo-Lamas.png"},
+				{id: 5, name: "Cub Swanson", win: 23,lose:7, height: "5'8", img: "./pics/FeatherWeight/Cub-Swanson.png"},
+				{id: 6, name: "Anthony Pettis", win: 19,lose:5, height: "5'10", img: "./pics/FeatherWeight/Anthony-Pettis.png"},
+				{id: 7, name: "Jeremy Stephens", win: 25,lose:13, height: "5'9", img: "./pics/FeatherWeight/Jeremy-Stephens.png"},
+				{id: 8, name: "Charles Oliviera", win: 21,lose:7, height: "5'10", img: "./pics/FeatherWeight/Charles-Oliveria.png"},
+				{id: 9, name: "Dennis Bermudez", win: 17,lose:5, height: "5'6", img: "./pics/FeatherWeight/Dennis-Bermudez.png"}
 			   
 			] ; 
 
- var AllLightWeights = 
+export var allLightWeights = 
               [ 
-			    {name: "Conor McGregor", record: "21-3-0", height: "5' 7", img: "./pics/usa.png"},
-				{name: "Jose Aldo", record: "26-2-0", record: "5'8", img: 5},
-				{name: "Frankie Edgar", record: "20-5-2", record: "5'6", img: 10},
-				{name: "Max Holloway", record: "16-3-0", record: "5'11", img: 4},
-				{name: "Ricardo Lamas", record: "17-5-0", record: "5'8", img: 6},
-				{name: "Cub Swanson", record: "232", record: "", img: 10},
-				{name: "Anthony Pettis", record: "155", record: "", img: 6},
-				{name: "Jeremy Stephens", record: "115", record: "", img: 6},
-				{name: "Charles Oliviera", record: "170", record: "", img: 7},
-				{name: "Dennis Bermudez", record: "180", record: "", img: 5}
+			    {id: 0, name: "(C)Conor McGregor", win: 21,lose: 3, height: "5' 7", img: "./pics/FeatherWeight/Conor-McGregor.png"},
+				{id: 1, name: "Khabib Nurmag", win: 24,lose:0, height: "5'10", img: "./pics/LightWeight/Khabib-Nurmagomedov.png"},
+				{id: 2, name: "Tony Ferguson", win: 23,lose: 3, height: "5'11", img: "./pics/LightWeight/Tony-Ferguson.png"},
+				{id: 3, name: "Eddie Alvarez", win: 28,lose:5, height: "5'9", img: "./pics/LightWeight/Eddie-Alvarez.png"},
+				{id: 4, name: "Rafael Dos Anjos", win: 25,lose:9, height: "5'9", img: "./pics/LightWeight/Rafael-Dos-Anjospng.png"},
+				{id: 5, name: "Nate Diaz", win: 20,lose:11, height: "6'0", img: "./pics/LightWeight/Nate-Diaz.png"},
+				{id: 6, name: "Edson Barboza", win: 18,lose:4, height: "5'11", img: "./pics/LightWeight/Edson-Barbosa.png"},
+				{id: 7, name: "Michael Johnson", win: 18,lose:11, height: "5'10", img: "./pics/LightWeight/Michael-Johnson.png"},
+				{id: 8, name: "Michael Chiesa", win: 14,lose:2, height: "6'11", img: "./pics/LightWeight/Michael-Chiesa.png"},
+				{id: 9, name: "Deneil Dariush", win: 14,lose:2, height: "5'10", img: "./pics/LightWeight/Beneil-Dariush.png"}
+			   
+			] ; 
+
+ export var allWelterWeights = 
+              [ 
+			    {id: 0, name: "(C)Tyron Woodley", win: 21, lose:3, height: "5' 7", img: "./pics/WelterWeight/T-W.png"},
+				{id: 1, name: "Stephen Thompson", win: 13,lose:1, height: "6'0", img: "./pics/WelterWeight/S-T"},
+				{id: 2, name: "Robbie Lawlor", win: 27,lose:11, height: "5'11", img: "./pics/WelterWeight/R-L.png"},
+				{id: 3, name: "Demian Maia", win: 24,lose:6, height: "6'1", img: "./pics/WelterWeight/D-M.png"},
+				{id: 4, name: "Carlos Condit", win: 30,lose:10, height: "6'2", img: "./pics/WelterWeight/C-C.png"},
+				{id: 5, name: "Donald Cerrone", win: 31,lose:7, height: "6'1", img: "./pics/WelterWeight/D-C.png"},
+				{id: 6, name: "Johny Hendricks", win: 17,lose:5, height: "5'9", img: "./pics/WelterWeight/J-H.png"},
+				{id: 7, name: "Lorenz Larkin", win: 18,lose:5, height: "5'11", img: "./pics/WelterWeight/L-L.png"},
+				{id: 8, name: "Kelvin Gastelum", win: 13,lose:2, height: "5'9", img: "./pics/WelterWeight/K-G.png"},
+				{id: 9, name: "Neil Magny", win: 18,lose:6, height: "6'3", img: "./pics/WelterWeight/N-M.png"}
 			   
 			] ; 
 
